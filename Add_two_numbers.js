e234505df1f931e515722c21c06b5f5c1704b926
lavanyaguru17 <lavanyaguru17@gmail.com>
2025-04-17 09:07:35 +0530

    first

---
+++ Add_two_numbers.js
@@ -0,0 +1,12 @@
+//compile time
+const num1=5;
+const num2=5;
+const result=num1+num2;
+console.log("The sum of",num1,"and",num2,"is:",result);
+
+
+//run time
+const num1=parseInt(prompt("Enter a number1: "));
+const num2=parseInt(prompt("Enter a number2: "));
+const result=num1+num2;
+console.log(`The sum of ${num1}and${num2} is:${result} `);
